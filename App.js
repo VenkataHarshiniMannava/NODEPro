6276dee85a18bdf0916f4b4b7f907fde38a7c1d8
VenkataHarshiniMannava <115424936+VenkataHarshiniMannava@users.noreply.github.com>
2023-11-30 16:17:58 +0530

    added

---
+++ App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import SlotBookingApp from './components/SlotBookingApp/SlotBookingApp'; // Adjust the path as per your project structure
+
+function App() {
+  return (
+    <div className="App">
+      <SlotBookingApp />
+    </div>
+  );
+}
+
+export default App;
